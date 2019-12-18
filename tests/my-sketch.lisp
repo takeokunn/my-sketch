bf1b7511be6695c3afe6734955e0d1995ad0d514
takeokunn <bararararatty@gmail.com>
2019-12-19 03:48:30 +0900

    Add test

--- tests/my-sketch.lisp
+++ tests/my-sketch.lisp
@@ -1,16 +1,41 @@
 (defpackage my-sketch-test
     (:use :cl :prove)
     (:import-from :my-sketch.math
-        :radians))
+        :clamp-1
+        :radians
+        :degrees)
+    (:import-from :my-sketch.utils
+        :pad-list))
 (in-package :my-sketch-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :my-sketch)' in your Lisp.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;          math           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(plan 5)
+
+;; clamp-1
+(is (my-sketch.math:clamp-1 10) 1.0)
+(is (my-sketch.math:clamp-1 0.5) 0.5)
+(is (my-sketch.math:clamp-1 -10) 0.0)
+
+;; radians
+(is (my-sketch.math:radians 180) PI)
+
+;; degrees
+(is (my-sketch.math:degrees PI) 180.0d0)
+
+(finalize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;         utils           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (plan 1)
 
-;; math
-(ok (eq 3 3))
-
-;; utils
+;; pad-list
+(is (my-sketch.utils:pad-list '(4 5) 1 5) '(1 1 1 4 5))
 
 (finalize)
