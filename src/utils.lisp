334db38c6316debeba3eaeb3b6e7dd60ecb53a82
takeokunn <bararararatty@gmail.com>
2019-12-21 01:38:44 +0900

    Update

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,12 +1,19 @@
 (defpackage my-sketch.utils
     (:use :cl :my-sketch.math)
     (:import-from :alexandria
-        :make-keyword)
+        :make-keyword
+        :lerp)
     (:export :pad-list
         :split-n
         :group
         :order-list
-        :mix-lists))
+        :mix-lists
+        :div2-inexact
+        :abs-or-rel
+        :lerp-lists
+        :flatten
+        :coerce-float
+        :relative-path))
 (in-package :my-sketch.utils)
 
 (defun pad-list (list pad length)
@@ -41,3 +48,43 @@
 
 (defun mix-lists (&rest lists)
     (apply #'append (apply #'mapcar #'list lists)))
+
+(defun div2-inexact (a)
+    (multiple-value-bind (x y)
+        (floor a 2)
+        (values x (+ x y))))
+
+(defun abs-or-rel (val src)
+    (if (numberp val)
+        (cond ((< 0 val 1) (* src val))
+            ((<= 1 val) val)
+            (t src))
+        (or src 0)))
+
+(defun lerp-lists (v list-a list-b)
+    (mapcar (lambda (a b) (alexandria:lerp v a b)) list-a list-b))
+
+(defun flatten (tree)
+    (let (list)
+        (labels ((traverse (subtree)
+                     (when subtree
+                         (if (consp subtree)
+                             (progn
+                                 (traverse (car subtree))
+                                 (traverse (cdr subtree)))
+                             (push subtree list)))))
+            (traverse tree))
+        (nreverse list)))
+
+(defun coerce-float (x)
+    (coerce x 'single-float))
+
+
+;; (defun copy-buffer (src dst length &key (src-offset 0) (dst-offset 0))
+;;     (declare (optimize (speed 3) (debug 0)))
+;;     (loop for i from 0 below length
+;;         do (setf (cffi:mem-aref dst :uint8 (+ i src-offset))
+;;                (cffi:mem-aref src :uint8 (+ i dst-offset)))))
+
+(defun relative-path (path &optional (system 'my-sketch))
+    (format nil "~a" (asdf:system-relative-pathname system path)))
