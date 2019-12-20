3d46fe5cf374c032938e0891d76ca7a0e17ad973
takeokunn <bararararatty@gmail.com>
2019-12-20 18:17:27 +0900

    Update

--- src/utils.lisp
+++ src/utils.lisp
@@ -2,7 +2,11 @@
     (:use :cl :my-sketch.math)
     (:import-from :alexandria
         :make-keyword)
-    (:export :pad-list))
+    (:export :pad-list
+        :split-n
+        :group
+        :order-list
+        :mix-lists))
 (in-package :my-sketch.utils)
 
 (defun pad-list (list pad length)
@@ -10,3 +14,30 @@
         list
         (append (make-list (- length (length list)) :initial-element pad)
             list)))
+
+(defun split-n (list n)
+    (when (>= (length list) n)
+        (loop with acc = '()
+            for i below n
+            do (setf acc (cons (car list) acc)
+                   list (cdr list))
+            finally (return (cons (nreverse acc) list)))))
+
+(defun group (list &optional (group-length 2))
+    (loop with acc = '()
+        while (or (not acc) (cdr list))
+        do (let ((split (split-n list group-length)))
+               (when (car split)
+                   (setf acc (cons (car split) acc)))
+               (setf list (cdr split)))
+        finally (return (nreverse acc))))
+
+;; TODO
+;; (defun group-bits ())
+
+(defun order-list (order list)
+    (loop for o in order
+        collect (nth o list)))
+
+(defun mix-lists (&rest lists)
+    (apply #'append (apply #'mapcar #'list lists)))
--- tests/my-sketch.lisp
+++ tests/my-sketch.lisp
@@ -5,7 +5,8 @@
         :radians
         :degrees)
     (:import-from :my-sketch.utils
-        :pad-list))
+        :pad-list
+        :split-n))
 (in-package :my-sketch-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :my-sketch)' in your Lisp.
@@ -33,9 +34,27 @@
 ;;         utils           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(plan 1)
+(plan 11)
 
 ;; pad-list
 (is (my-sketch.utils:pad-list '(4 5) 1 5) '(1 1 1 4 5))
+
+;; split-n
+(is (my-sketch.utils:split-n '(1 2 3 4 5) 1) '((1) 2 3 4 5))
+(is (my-sketch.utils:split-n '(1 2 3 4 5) 3) '((1 2 3) 4 5))
+
+;; group
+(is (my-sketch.utils:group '(1 2 3 4 5)) '((1 2) (3 4)))
+(is (my-sketch.utils:group '(1 2 3 4 5) 3) '((1 2 3)))
+(is (my-sketch.utils:group '(1 2 3 4 5 6) 2) '((1 2) (3 4) (5 6)))
+(is (my-sketch.utils:group '(1 2 3 4 5 6) 3) '((1 2 3) (4 5 6)))
+
+;; order-list
+(is (my-sketch.utils:order-list '(0) '("a" "b" "c")) '("a"))
+(is (my-sketch.utils:order-list '(0 2) '("a" "b" "c")) '("a" "c"))
+
+;; mix-lists
+(is (my-sketch.utils:mix-lists '((1 2 3) (a b c))) '(1 a 2 b 3 c))
+(is (my-sketch.utils:mix-lists '((1 2) (a b) (x y))) '(1 a x 2 b y))
 
 (finalize)
